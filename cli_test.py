ed409dca351aac1f21870bdacfbe2c8041903def
Phillip Hocking <phillhocking@gmail.com>
2021-09-02 23:30:34 -0700

    Retooling tests with os.getcwd()

--- cli_test.py
+++ cli_test.py
@@ -5,17 +5,17 @@ import sys
 
 import pytest
 
-cwd = subprocess.call("pwd", shell=True)
+cwd = os.getcwd()
 print(cwd)
 
 
 def test_entrypoint():
     """Test entry point exit 2 for usage."""
-    exit_status = subprocess.call("package_statistics")
+    exit_status = subprocess.call(cwd + "/package_statistics.py")
     assert exit_status == 2
 
 
 def test_validarch():
     """Test with valid arch argument."""
-    exit_status = subprocess.call(["package_statistics", "amd64"])
+    exit_status = subprocess.call([cwd + "/package_statistics.py", "amd64"])
     assert exit_status == 0
